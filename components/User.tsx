d06dfc92181be569ea30ca5102706c3b674079ec
erDeep640390 <erdeep64930@gmail.com>
2024-06-24 17:44:37 +0530

    adding desturcturing

--- components/User.tsx
+++ components/User.tsx
@@ -1,11 +1,11 @@
 
 
-const User = (props:{name:string; age:number;isStudent:boolean}) => {
+const User = ({name,age,isStudent}:{name:string; age:number;isStudent:boolean}) => {
   return (
     <div>
-        <h1>{props.name}</h1>
-        <h1>{props.age}</h1>
-        <h1>{props.isStudent}</h1>
+        <h1>{name}</h1>
+        <h1>{age}</h1>
+        <h1>{isStudent}</h1>
        
     </div>
   )
