c2238c35542b1fbc72ac48b72b2732bd22aca95c
erDeep640390 <erdeep64930@gmail.com>
2024-06-24 17:49:51 +0530

    adding interface to the type

--- components/User.tsx
+++ components/User.tsx
@@ -1,14 +1,17 @@
-type UserShape= {name:string; age:number;isStudent:boolean}
-
-const User = ({name,age,isStudent}:UserShape) => {
-  return (
-    <div>
-        <h1>{name}</h1>
-        <h1>{age}</h1>
-        <h1>{isStudent}</h1>
-       
-    </div>
-  )
+interface UserShape {
+  name: string;
+  age: number;
+  isStudent: boolean;
 }
 
-export default User
+const User = ({ name, age, isStudent }: UserShape) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{isStudent}</h1>
+    </div>
+  );
+};
+
+export default User;
